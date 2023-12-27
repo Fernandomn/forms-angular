82f859986b72d5c0e40ad6fc3e60358858cc90e8
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2023-12-27 13:20:58 -0300

    validating Name and Email form fields

---
+++ src/app/cadastro/cadastro.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-cadastro',
+  templateUrl: './cadastro.component.html',
+  styleUrls: ['./cadastro.component.css'],
+})
+export class CadastroComponent implements OnInit {
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {}
+
+  cadastrar(form: NgForm) {
+    if (form.valid) {
+      this.router.navigate(['/sucesso']);
+    } else {
+      alert('Formulário inválido!');
+    }
+    console.log('Formulário enviado', form);
+    console.log('Form controls', form.controls);
+  }
+}
