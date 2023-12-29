a3d435d0f51212ab82ea92e8bb041b90af0a29d5
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2023-12-28 22:30:09 -0300

    creating FullAgeDirective

---
+++ src/app/directives/full-age.directive.spec.ts
@@ -0,0 +1,8 @@
+import { FullAgeDirective } from './full-age.directive';
+
+describe('FullAgeDirective', () => {
+  it('should create an instance', () => {
+    const directive = new FullAgeDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/directives/full-age.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appFullAge]'
+})
+export class FullAgeDirective {
+
+  constructor() { }
+
+}
