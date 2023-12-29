0366224864726c3b101b806a97fd35ad8180ed73
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2023-12-28 23:07:50 -0300

    implementing FullAgeValidator, directive who checks if user is over 18yo

--- src/app/directives/full-age.directive.ts
+++ src/app/directives/full-age.directive.ts
@@ -1,10 +1,27 @@
 import { Directive } from '@angular/core';
+import {
+  AbstractControl,
+  NG_VALIDATORS,
+  ValidationErrors,
+  Validator,
+} from '@angular/forms';
 
 @Directive({
-  selector: '[appFullAge]'
+  selector: '[fullAgeValidator]',
+  providers: [
+    { provide: NG_VALIDATORS, useExisting: FullAgeDirective, multi: true },
+  ],
 })
-export class FullAgeDirective {
+export class FullAgeDirective implements Validator {
+  constructor() {}
 
-  constructor() { }
+  validate(control: AbstractControl<any, any>): ValidationErrors | null {
+    const birthDate = control.value;
+    const birthYear = new Date(birthDate).getFullYear();
+    const currentYear = new Date().getFullYear();
 
+    const isBigger = birthYear + 18 <= currentYear;
+
+    return isBigger ? null : { fullAgeValidator: true };
+  }
 }
