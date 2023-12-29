604fe9790000a9d4aac646550155ec345d800e6b
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2023-12-29 10:44:07 -0300

    using CepService on Cadastro form

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ConsultaCepService } from '../services/consulta-cep.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -8,7 +9,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./cadastro.component.css'],
 })
 export class CadastroComponent implements OnInit {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private cepService: ConsultaCepService) {}
 
   ngOnInit(): void {}
 
@@ -20,5 +21,25 @@ export class CadastroComponent implements OnInit {
     }
     console.log('Formulário enviado', form);
     console.log('Form controls', form.controls);
+  }
+
+  checkCep(ev: any, f: NgForm) {
+    const cep = ev?.target?.value;
+    if (cep) {
+      this.cepService.checkCep(cep).subscribe((result) => {
+        console.log(result);
+        this.populateAddress(result, f);
+      });
+    }
+  }
+
+  private populateAddress(data: any, f: NgForm) {
+    f.form.patchValue({
+      endereco: data.logradouro,
+      complement: data.complemento,
+      bairro: data.bairro,
+      cidade: data.localidade,
+      estado: data.uf,
+    });
   }
 }
