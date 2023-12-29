6a737006e1672afe91ce90c0862e8e841d7aac72
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2023-12-29 10:43:53 -0300

    creating ValidatingCepDirective

---
+++ src/app/directives/validating-cep.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ValidatingCepDirective } from './validating-cep.directive';
+
+describe('ValidatingCepDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ValidatingCepDirective();
+    expect(directive).toBeTruthy();
+  });
+});
