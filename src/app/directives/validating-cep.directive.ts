2706d424fe65d9c7c751ea3f8ad67f5804cc11fe
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2024-01-02 15:32:32 -0300

    implementing cepValidator

--- src/app/directives/validating-cep.directive.ts
+++ src/app/directives/validating-cep.directive.ts
@@ -1,10 +1,35 @@
 import { Directive } from '@angular/core';
+import {
+  AbstractControl,
+  AsyncValidator,
+  NG_ASYNC_VALIDATORS,
+  ValidationErrors,
+} from '@angular/forms';
+import { Observable, map } from 'rxjs';
+import { ConsultaCepService } from '../services/consulta-cep.service';
 
 @Directive({
-  selector: '[appValidatingCep]'
+  selector: '[cepValidator]',
+  providers: [
+    {
+      provide: NG_ASYNC_VALIDATORS,
+      useExisting: ValidatingCepDirective,
+      multi: true,
+    },
+  ],
 })
-export class ValidatingCepDirective {
+export class ValidatingCepDirective implements AsyncValidator {
+  constructor(private cepService: ConsultaCepService) {}
 
-  constructor() { }
+  validate(
+    control: AbstractControl
+  ): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
+    const cep = control.value;
 
+    return this.cepService
+      .checkIfCepIsValid(cep)
+      .pipe(
+        map((result: any) => (result.erro ? { cepValidator: true } : null))
+      );
+  }
 }
